7cb91b1ea745a33df36dcd217bdb72bf70f93247
Ítalo Tabatinga <italotabatinga@gmail.com>
2024-12-08 22:26:47 -0300

    add 2024.8

---
+++ 2024/solutions/8.rb
@@ -0,0 +1,75 @@
+require_relative "problem_base"
+require_relative "collections"
+
+class Problem8 < ProblemBase
+  def problem = 8
+
+  def build(part)
+    @map = []
+    @antennas = {}
+    @antinodes = Set.new
+    @i_size = 0
+    @j_size = 0
+    input = (part == 1) ? input1 : input2
+    input.each_line.each_with_index do |line, i|
+      line[...-1].chars.each_with_index do |char, j|
+        @j_size = j if j > @j_size
+        next if char == '.'
+        @antennas[char] ||= []
+        @antennas[char].push(Vec2.new(i, j))
+      end
+      @i_size = i if i > @i_size
+    end
+
+    @i_size += 1
+    @j_size += 1
+  end
+
+  def part1
+    build(1)
+
+    @antennas.each do |_, antennas|
+      antennas[...-1].each_with_index do |ant_a, i|
+        antennas[i+1..].each_with_index do |ant_b, j|
+          diff = ant_b - ant_a
+          first_antinode = ant_b + diff
+          second_antinode = ant_a - diff
+          potential_antinodes = [ant_b + diff, ant_a - diff]
+          potential_antinodes.each do |potential_antinode|
+            if potential_antinode.x >= 0 && potential_antinode.x < @i_size && potential_antinode.y >= 0 && potential_antinode.y < @j_size
+              @antinodes.add(potential_antinode)
+            end
+          end
+        end
+      end
+    end
+
+    @antinodes.size
+  end
+
+  def part2
+    build(2)
+
+    @antennas.each do |_, antennas|
+      antennas[...-1].each_with_index do |ant_a, i|
+        antennas[i+1..].each_with_index do |ant_b, j|
+          diff = ant_b - ant_a
+          init_pos = ant_a - diff
+          @antinodes.add(ant_a)
+          @antinodes.add(ant_b)
+          while init_pos.x >= 0 && init_pos.x < @i_size && init_pos.y >= 0 && init_pos.y < @j_size
+            @antinodes.add(init_pos)
+            init_pos -= diff
+          end
+          init_pos = ant_b + diff
+          while init_pos.x >= 0 && init_pos.x < @i_size && init_pos.y >= 0 && init_pos.y < @j_size
+            @antinodes.add(init_pos)
+            init_pos += diff
+          end
+        end
+      end
+    end
+
+    @antinodes.size
+  end
+end
---
+++ 2024/solutions/collections.rb
@@ -0,0 +1,24 @@
+class Vec2
+  attr_reader :x, :y
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+
+  def initialize_dup(other) = new(other.x, other.y)
+
+  def to_s = "(#{x}, #{y})"
+
+  alias_method :inspect, :to_s
+
+  def +(other) = Vec2.new(@x + other.x, @y + other.y)
+
+  def -(other) = Vec2.new(@x - other.x, @y - other.y)
+
+  def ==(other) = @x == other.x && @y == other.y
+
+  alias_method :eql?, :==
+
+  def hash = [x, y].hash
+end
